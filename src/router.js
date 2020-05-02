b179da68fd322882171d99a2b295a590898821fd
LiYaaaaang <445410409@qq.com>
2020-05-02 16:12:20 +0800

    添加页面组件和图片资源

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const routes = [
+
+]
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     devServer: {
         open: true,
+        host: 'localhost',
+        port: 8080,
+
     }
 }
