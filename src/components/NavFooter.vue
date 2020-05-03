31a9e1bd8d61cb45cd619a791c830e7694e0d85e
LiYaaaaang <445410409@qq.com>
2020-05-03 19:45:13 +0800

    完成底部导航栏

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,30 +1,65 @@
 <template>
-	<div class="footer">
-		<div class="footer-logo">
-			<img src="/imgs/logo-footer.png" alt="" />
-			<p>小米商城</p>
-		</div>
-		<div class="footer-link">
-			<a href="">一人之下</a><span>|</span> <a href="">武当王也</a><span>|</span> <a href="">不摇碧莲张楚岚</a
-			><span>|</span>
-			<a href="">跟对大姐大，吃喝都不怕 </a>
-		</div>
-		<div class="copyright">Copyright ©2019 mi.futurefe.com All Rights Reserved.</div>
-	</div>
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt />
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="javascript:;">一人之下</a>
+            <span>|</span>
+            <a href="javascript:;">武当王也</a>
+            <span>|</span>
+            <a href="javascript:;">不摇碧莲张楚岚</a>
+            <span>|</span>
+            <a href="javascript:;">跟对大姐大，吃喝都不怕</a>
+        </div>
+        <div class="copyright">
+            Copyright ©2019
+            <span class="domain">mi.futurefe.com</span>
+            All Rights Reserved.
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'nav-footer',
-	data() {
-		return {};
-	},
+    name: "nav-footer",
+    data() {
+        return {};
+    }
 };
 </script>
 <style lang="scss" scoped>
 .footer {
-	height: 234px;
-	border-top: 4px solid #ff6600;
-	background-color: #333333;
+    height: 234px;
+    border-top: 4px solid #ff6600;
+    background-color: #333333;
+    color: #999999;
+    font-size: 16px;
+    text-align: center;
+    .footer-logo {
+        margin-top: 46px;
+        margin-bottom: 31px;
+        img {
+            width: 53px;
+            height: 36px;
+            margin-bottom: 13px;
+        }
+    }
+    .footer-link {
+        a {
+            color: #999999;
+            display: inline-block;
+        }
+        span {
+            margin: 0 10px;
+        }
+    }
+    .copyright {
+        margin-top: 13px;
+        .domain {
+            color: #ff6600;
+        }
+    }
 }
 </style>
